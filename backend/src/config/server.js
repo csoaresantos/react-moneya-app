4966d02ddf8db3361b13e4aaadecb3153a5c1a0c
Charles <charlessoaresdossantos@gmail.com>
2018-08-23 23:33:52 -0300

    backend finzalizado.

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,14 @@
 const port = 3003
+const allowsCors = require('./cors')
 const bodyParse = require('body-parser')
 const express = require('express')
+const queryParser = require('express-query-int')
 const server = express()
 
 server.use(bodyParse.urlencoded({ extended: true }))
 server.use(bodyParse.json())
+server.use(allowsCors)
+server.use(queryParser)
 
 server.listen(port, function(){
     console.log(`BACKEND IS RUNNING ON PORT ${port}.`)

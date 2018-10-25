3ce0fbd79ce0e5929aa25bd4bbd2faeb82f65284
Charles <charlessoaresdossantos@gmail.com>
2018-10-24 23:21:15 -0300

    principal application is done

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,8 +5,16 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { init } from './billingCycleActions'
 import ItemList from './itemList'
-
+import Summary from './summary'
 class BillingCycleForm extends Component {
+
+    calculateSummary() {
+        const sum = (t, v) => t+v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(c => +c.value || 0).reduce(sum)
+        }
+    }
 
     render() {
         const { handleSubmit, credits, debts } = this.props
@@ -16,6 +24,7 @@ class BillingCycleForm extends Component {
                     <Field name="name" component={labelAndInput} label='Nome'cols='12 4' placeholder='Informe o nome' />
                     <Field name="month" component={labelAndInput} label='Mês'cols='12 4' placeholder='Informe o mês'  />
                     <Field name="year" component={labelAndInput} label='Ano'cols='12 4' placeholder='Informe o ano'  />
+                    <Summary credit={this.calculateSummary().sumOfCredits} debt={this.calculateSummary().sumOfDebts} />
                     <ItemList cols='12 6' list={credits} field='credits' legend='Créditos' />
                     <ItemList cols='12 6' list={debts} field='debts' legend='Débitos' showStatus={true} />
                 </div>
